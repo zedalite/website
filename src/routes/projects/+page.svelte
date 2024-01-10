95c46f4c0e9352ac76af70cffb85fad53dc56317
Kuhney <42678994+Kuhney@users.noreply.github.com>
2024-01-10 20:51:59 +0100

    projects card update

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -6,20 +6,16 @@
 </script>
 
 <div class="container mx-auto flex justify-center gap-8 p-8">
-    <a href="/projects/quote-your-friends" class="card bg-initial card-hover">
+    <a href="/projects/quote-your-friends" class="card bg-initial card-hover size-1/3">
         <header>
             <img src={appIcon} class="w-full" alt="App-Icon"/>
         </header>
         <div class="p-4">
             <h3 class="h3">Quote Your Friends</h3>
-            <article>A app to share quotes between friends in a closed group</article>
+            <article>A app to share quotes between friends</article>
         </div>
-        
-        <footer>
-            <p>FOOTER</p>
-        </footer>
     </a>
-    <a href="/projects/quote-your-friends" class="card bg-initial card-hover">
+    <div class="card bg-initial card-hover size-1/3">
         <header>
             <!-- <img src={appIcon} class="w-full" alt="App-Icon"/> -->
         </header>
@@ -27,11 +23,7 @@
             <h3 class="h3">Coming...</h3>
             <article>The next project in development</article>
         </div>
-        
-        <footer>
-            <p>FOOTER</p>
-        </footer>
-    </a>
+    </div>
 </div>
 
 
