8744d714041624581f4d2b935ffcc41ad5ffcf5b
Kuhney <42678994+Kuhney@users.noreply.github.com>
2024-01-10 21:36:35 +0100

    tiny rearranging the homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,7 @@
     export let data: PageData;
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Hello, We are Zedalite!</h1>
-		<p>A Team of 3 people who develop apps</p>
-	</div>
+<div class="container h-full mx-auto flex flex-col gap-8 justify-center items-center">
+	<h1 class="h1">Hello, We are Zedalite!</h1>
+	<p>A Team of 3 people who develop apps and stuff</p>
 </div>
