46ffb07310943089cff1775a3be947119721f0d9
Kuhney <42678994+Kuhney@users.noreply.github.com>
2024-01-10 19:54:54 +0100

    added privacy-policy

--- src/routes/projects/quote-your-friends/+page.svelte
+++ src/routes/projects/quote-your-friends/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { PageData } from "./$types";
+  import screenshot from "$lib/assets/app_example.jpg";
 
   export let data: PageData;
 </script>
@@ -15,12 +16,14 @@
       <span class="material-symbols-outlined"> keyboard_backspace </span>
       <span>Browse</span>
     </a>
-    <a href="https://zedalite.de/#/login" target="_blank" rel="noreferrer noopener" class="btn variant-ghost">
+    <a href="https://quotes.zedalite.de/" target="_blank" rel="noreferrer noopener" class="btn variant-ghost">
       <span>Launch</span>
       <span class="material-symbols-outlined"> rocket_launch </span>
     </a>
   </div>
+  <img class="max-w-xs rounded-lg" src={screenshot} alt="App screenshot" height="100vh" />
   <p>
     Explanation
   </p>
+  <a href="/projects/quote-your-friends/privacy-policy" class="btn variant-ghost">Privacy Policy</a>
 </div>
