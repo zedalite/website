a1d42dda986ac0c9da2c6d447d02434a67aaadf1
Kuhney <42678994+Kuhney@users.noreply.github.com>
2024-01-10 21:36:00 +0100

    better about us section

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
+  import { Avatar } from "@skeletonlabs/skeleton";
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">About Us</h1>
-		<ul>
-			<li><p>Jens</p></li>
-            <li><p>Niclas</p></li>
-            <li><p>Simon</p></li>
-		</ul>
-	</div>
+<div class="container h-full mx-auto flex flex-col">
+	<br/>
+  <h1 class="h1 text-center">About Us</h1>
+  <br/>
+  <div class="flex gap-8 justify-center">
+    <div class="flex flex-col items-center w-modal-slim card p-8 card-hover">
+      <Avatar initials="JT" background="bg-primary-500" />
+      <h2 class="h2">Jens</h2>
+      <p>Co-Founder</p>
+    </div>
+    <div class="flex flex-col items-center w-modal-slim card p-8 card-hover">
+      <Avatar initials="NK" background="bg-secondary-500" />
+      <h2 class="h2">Niclas</h2>
+      <p>Co-Founder</p>
+    </div>
+    <div class="flex flex-col items-center w-modal-slim card p-8 card-hover">
+      <Avatar initials="SF" background="bg-tertiary-500" />
+      <h2 class="h2">Simon</h2>
+      <p>Developer</p>
+    </div>
+  </div>
 </div>
