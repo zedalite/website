78b99baed0251512d3a0fee1a96bd9a84362da31
Kuhney <42678994+Kuhney@users.noreply.github.com>
2024-01-16 11:35:18 +0100

    New Homepage Sentence

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,5 +8,5 @@
 
 <div class="container h-full mx-auto flex flex-col gap-8 justify-center items-center">
 	<h1 class="h1">Hello, We are Zedalite!</h1>
-	<p>A Team of 3 people who develop apps and stuff</p>
+	<p>A creative trio, united by passion and code, on a mission to craft innovative apps and turn digital dreams into reality</p>
 </div>
