714051ce98e9045dac10d660cef4ad6cd687d8f8
Kuhney <42678994+Kuhney@users.noreply.github.com>
2024-01-16 11:35:40 +0100

    added linkedin link

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,17 +9,17 @@
 	<br/>
   <h1 class="h1 text-center">About Us</h1>
   <br/>
-  <div class="flex gap-8 justify-center">
-    <div class="flex flex-col items-center w-modal-slim card p-8 card-hover">
+  <div class="flex gap-8 justify-center flex-wrap">
+    <a href="https://www.linkedin.com/in/jensthewes/" target="_blank" class="flex flex-col items-center w-modal-slim card p-8 card-hover">
       <Avatar initials="JT" background="bg-primary-500" />
-      <h2 class="h2">Jens</h2>
+      <h2 class="h2">Jens Thewes</h2>
       <p>Co-Founder</p>
-    </div>
-    <div class="flex flex-col items-center w-modal-slim card p-8 card-hover">
+    </a>
+    <a href="https://www.linkedin.com/in/niclas-kuhn-996ba6194/" target="_blank" class="flex flex-col items-center w-modal-slim card p-8 card-hover">
       <Avatar initials="NK" background="bg-secondary-500" />
-      <h2 class="h2">Niclas</h2>
+      <h2 class="h2">Niclas Kuhn</h2>
       <p>Co-Founder</p>
-    </div>
+    </a>
     <div class="flex flex-col items-center w-modal-slim card p-8 card-hover">
       <Avatar initials="SF" background="bg-tertiary-500" />
       <h2 class="h2">Simon</h2>
