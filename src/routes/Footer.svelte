50977ee8cc622a1abc2736c94fa1203f7609c5f1
Kuhney <42678994+Kuhney@users.noreply.github.com>
2024-01-16 11:37:24 +0100

    Added Footer with PrivacyPolicy & TermsOfService

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import '../app.postcss';
+</script>
+
+
+<footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="/" class="hover:underline">Zedalite</a>. All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+            <a href="/privacy-policy" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+        </li>
+        <li>
+            <a href="/terms-of-service" class="hover:underline me-4 md:me-6">Terms of Service</a>
+        </li>
+        <!-- 
+        <li>
+            <a href="#" class="hover:underline">Contact</a>
+        </li> -->
+    </ul>
+    </div>
+</footer>
